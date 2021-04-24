900cd3377a4264697cf71605d5e1f730649664f7
snehalupatil <patilsnehal4575@gmail.com>
2021-04-24 13:20:43 +0530

    Added: UC 1 functionality is added

--- Empwage.java
+++ Empwage.java
@@ -1,6 +1,7 @@
 class Empwage{
         public static void main(String args[]){
                 System.out.println("Welcome to java program");
+		System.out.println("Branch 1 code is Added");
 }
 
 }
