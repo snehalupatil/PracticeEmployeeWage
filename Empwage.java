b8b0a191a668c8c103894875737086b62d07a1ad
snehalupatil <48207533+snehalupatil@users.noreply.github.com>
2021-04-24 13:39:46 +0530

    Added remote code

--- Empwage.java
+++ Empwage.java
@@ -2,6 +2,7 @@ class Empwage{
         public static void main(String args[]){
                 System.out.println("Welcome to java program");
 		System.out.println("Branch 1 code is Added");
+		System.out.println("Remote code is Added");
 		System.out.println("Branch 2 code is Added");
 }
 
